4cf089e783564185235670b6e2bd886bafaf3f1a
Marcos Vinicius Lemos da Silva <1110032@senacminas.edu.br>
2026-06-25 15:11:29 -0300

    v0.6

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = windows.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://natacaoprojeto.onrender.com";
+const API = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://natacaoprojeto.onrender.com";
 
 async function cadastrarAluno(event) {
 
